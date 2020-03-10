c2465f0a856d602a8ed83629b168cd770ecc36eb
metaxiii <gael.lehchibi@outlook.com>
2020-03-10 16:23:19 +0100

    end of course

--- src/main/java/com/openclassrooms/testing/Calculator.java
+++ src/main/java/com/openclassrooms/testing/Calculator.java
@@ -2,8 +2,16 @@ package com.openclassrooms.testing;
 
 public class Calculator {
 
-	public int add(int a, int b) {
-		return a + b;
-	}
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int substract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
 
 }
--- src/test/java/com/openclassrooms/testing/CalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/CalculatorTest.java
@@ -5,19 +5,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
+	private int nombre1;
+	private int nombre2;
+	Calculator calculatrice = new Calculator();
 
 	@Test
-	void testAddTwoPositiveNumbers() {
-		// Arrange
-		int a = 2;
-		int b = 3;
-		Calculator calculator = new Calculator();
-
-		// Act
-		int somme = calculator.add(a, b);
-
-		// Assert
-		assertEquals(5, somme);
+	void shouldAddTwoNumber() {
+		nombre1 = 2;
+		nombre2 = 3;
+		assertEquals(5, calculatrice.add(nombre1, nombre2));
 	}
 
+	@Test
+	void substract() {
+		nombre1 = 10;
+		nombre2 = 5;
+		assertEquals(5, calculatrice.substract(nombre1, nombre2));
+	}
+
+	@Test
+	void multiply() {
+		nombre1 = 3;
+		nombre2 = 3;
+		assertEquals(9, calculatrice.multiply(nombre1, nombre2));
+	}
 }
