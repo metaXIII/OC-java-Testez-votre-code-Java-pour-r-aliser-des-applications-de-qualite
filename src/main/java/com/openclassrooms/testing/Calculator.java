b330e3d3248ef56aa0370dfb2d7c6351528c9d1c
metaxiii <gael.lehchibi@outlook.com>
2020-03-10 16:44:30 +0100

    add assertJ to replace Junit

--- src/main/java/com/openclassrooms/testing/Calculator.java
+++ src/main/java/com/openclassrooms/testing/Calculator.java
@@ -1,21 +1,33 @@
 package com.openclassrooms.testing;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Calculator {
 
-	public int add(int a, int b) {
-		return a + b;
-	}
+    public int add(int a, int b) {
+        return a + b;
+    }
 
-	public int multiply(int a, int b) {
-		return a * b;
-	}
+    public int multiply(int a, int b) {
+        return a * b;
+    }
 
-	public void longCalculation() {
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    public void longCalculation() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public Set<Integer> digitsSet(int number) {
+        Set<Integer> integers = new HashSet<>();
+        String       integer  = String.valueOf(number);
+        for (int i = 0; i < integer.length(); i++) {
+            if (integer.charAt(i) != '-')
+                integers.add(Integer.parseInt(integer, i, i + 1, 10));
+        }
+        return integers;
+    }
 }
--- src/test/java/com/openclassrooms/testing/CalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/CalculatorTest.java
@@ -1,113 +1,138 @@
 package com.openclassrooms.testing;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.text.MessageFormat;
-import java.time.Duration;
-import java.time.Instant;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
 
-	private static Instant startedAt;
+    private static Instant startedAt;
 
-	private Calculator calculatorUnderTest;
+    private Calculator calculatorUnderTest;
 
-	@BeforeEach
-	public void initCalculator() {
-		System.out.println("Appel avant chaque test");
-		calculatorUnderTest = new Calculator();
-	}
+    @BeforeEach
+    public void initCalculator() {
+        System.out.println("Appel avant chaque test");
+        calculatorUnderTest = new Calculator();
+    }
 
-	@AfterEach
-	public void undefCalculator() {
-		System.out.println("Appel après chaque test");
-		calculatorUnderTest = null;
-	}
+    @AfterEach
+    public void undefCalculator() {
+        System.out.println("Appel après chaque test");
+        calculatorUnderTest = null;
+    }
 
-	@BeforeAll
-	public static void initStartingTime() {
-		System.out.println("Appel avant tous les tests");
-		startedAt = Instant.now();
-	}
+    @BeforeAll
+    public static void initStartingTime() {
+        System.out.println("Appel avant tous les tests");
+        startedAt = Instant.now();
+    }
 
-	@AfterAll
-	public static void showTestDuration() {
-		System.out.println("Appel après tous les tests");
-		Instant endedAt = Instant.now();
-		long duration = Duration.between(startedAt, endedAt).toMillis();
-		System.out.println(MessageFormat.format("Durée des tests : {0} ms", duration));
+    @AfterAll
+    public static void showTestDuration() {
+        System.out.println("Appel après tous les tests");
+        Instant endedAt  = Instant.now();
+        long    duration = Duration.between(startedAt, endedAt).toMillis();
+        System.out.println(MessageFormat.format("Durée des tests : {0} ms", duration));
+    }
+
+    @Test
+    public void testAddTwoPositiveNumbers() {
+        // Arrange
+        int a = 2;
+        int b = 3;
+
+        // Act
+        int somme = calculatorUnderTest.add(a, b);
+
+        // Assert
+        assertThat(somme).isEqualTo(5);
+    }
+
+    @Test
+    public void multiply_shouldReturnTheProduct_ofTwoIntegers() {
+        // Arrange
+        int a = 42;
+        int b = 11;
+
+        // Act
+        int produit = calculatorUnderTest.multiply(a, b);
+
+        // Assert
+        assertThat(produit).isEqualTo(462);
+    }
+
+    @ParameterizedTest(name = "{0} x 0 doit être égal à 0")
+    @ValueSource(ints = {1, 2, 42, 1011, 5089})
+    public void multiply_shouldReturnZero_ofZeroWithMultipleIntegers(int arg) {
+        // Arrange -- Tout est prêt !
+
+        // Act -- Multiplier par zéro
+        int actualResult = calculatorUnderTest.multiply(arg, 0);
+
+        // Assert -- ça vaut toujours zéro !
+        assertThat(actualResult).isEqualTo(0);
+    }
+
+    @ParameterizedTest(name = "{0} + {1} doit être égal à {2}")
+    @CsvSource({"1,1,2", "2,3,5", "42,57,99"})
+    public void add_shouldReturnTheSum_ofMultipleIntegers(int arg1, int arg2, int expectResult) {
+        // Arrange -- Tout est prêt !
+
+        // Act
+        int actualResult = calculatorUnderTest.add(arg1, arg2);
+        // Assert
+        assertThat(expectResult).isEqualTo(actualResult);
+    }
+
+    @Timeout(1)
+    @Test
+    public void longCalcul_shouldComputeInLessThan1Second() {
+        // Arrange
+
+        // Act
+        calculatorUnderTest.longCalculation();
+
+        // Assert
+        // ...
+    }
+
+    @Test
+    public void digitsSet_shouldReturnsTheSetOfDigits_ofPositiveInteger() {
+        // GIVEN
+        int number = 95897;
+        // WHEN
+        Set<Integer> actualDigits   = calculatorUnderTest.digitsSet(number);
+        assertThat(actualDigits).containsExactlyInAnyOrder(5, 7, 8, 9);
+    }
+
+
+	@Test
+	public void digitsSet_shouldReturnsTheSetOfDigits_ofNegativeInteger() {
+		// GIVEN
+		int number = -42004;
+		// WHEN
+		Set<Integer> actualDigits   = calculatorUnderTest.digitsSet(number);
+		assertThat(actualDigits).containsExactlyInAnyOrder(0,2,4);
 	}
 
 	@Test
-	public void testAddTwoPositiveNumbers() {
-		// Arrange
-		int a = 2;
-		int b = 3;
-
-		// Act
-		int somme = calculatorUnderTest.add(a, b);
-
-		// Assert
-		assertEquals(5, somme);
+	public void digitsSet_shouldReturnsTheSetOfDigits_ofZeroInteger() {
+		// GIVEN
+		int number = 0;
+		// WHEN
+		Set<Integer> actualDigits   = calculatorUnderTest.digitsSet(number);
+		assertThat(actualDigits).containsExactly(0);
 	}
-
-	@Test
-	public void multiply_shouldReturnTheProduct_ofTwoIntegers() {
-		// Arrange
-		int a = 42;
-		int b = 11;
-
-		// Act
-		int produit = calculatorUnderTest.multiply(a, b);
-
-		// Assert
-		assertEquals(462, produit);
-	}
-
-	@ParameterizedTest(name = "{0} x 0 doit être égal à 0")
-	@ValueSource(ints = { 1, 2, 42, 1011, 5089 })
-	public void multiply_shouldReturnZero_ofZeroWithMultipleIntegers(int arg) {
-		// Arrange -- Tout est prêt !
-
-		// Act -- Multiplier par zéro
-		int actualResult = calculatorUnderTest.multiply(arg, 0);
-
-		// Assert -- ça vaut toujours zéro !
-		assertEquals(0, actualResult);
-	}
-
-	@ParameterizedTest(name = "{0} + {1} doit être égal à {2}")
-	@CsvSource({ "1,1,2", "2,3,5", "42,57,99" })
-	public void add_shouldReturnTheSum_ofMultipleIntegers(int arg1, int arg2, int expectResult) {
-		// Arrange -- Tout est prêt !
-
-		// Act
-		int actualResult = calculatorUnderTest.add(arg1, arg2);
-
-		// Assert
-		assertEquals(expectResult, actualResult);
-	}
-
-	@Timeout(1)
-	@Test
-	public void longCalcul_shouldComputeInLessThan1Second() {
-		// Arrange
-
-		// Act
-		calculatorUnderTest.longCalculation();
-
-		// Assert
-		// ...
-	}
-
 }
