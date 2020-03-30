af63aaf6f8fcbcf84484d7d1b58b474a0713c143
meta <gael.lehchibi@outlook.com>
2020-03-30 17:54:55 +0200

    Ajout d'un test d'intégration

---
+++ src/test/java/com/openclassrooms/testing/calcul/controller/CalculatorControllerTest.java
@@ -0,0 +1,52 @@
+package com.openclassrooms.testing.calcul.controller;
+
+import com.openclassrooms.testing.calcul.domain.Calculator;
+import com.openclassrooms.testing.calcul.service.CalculatorService;
+import com.openclassrooms.testing.calcul.service.SolutionFormatter;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import javax.inject.Inject;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+
+@WebMvcTest(controllers = {CalculatorController.class, CalculatorService.class})
+@ExtendWith(SpringExtension.class)
+class CalculatorControllerTestSIT {
+
+    @Inject
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SolutionFormatter solutionFormatter;
+
+    @MockBean
+    private Calculator calculator;
+
+    @Test
+    public void givenACalculatorApp_whenRequestToAdd_thenSolutionIsShown() throws Exception {
+        Mockito.when(calculator.add(2, 3)).thenReturn(5);
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders
+                                 .post("/calculator")
+                                 .param("leftArgument", "2")
+                                 .param("rightArgument", "3")
+                                 .param("calculationType", "ADDITION"))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andReturn();
+        assertThat(result.getResponse().getContentAsString())
+                .contains("id=\"solution\"")
+                .contains(">5</span>");
+        verify(calculator).add(2,3);
+        verify(solutionFormatter).format(5);
+    }
+}
