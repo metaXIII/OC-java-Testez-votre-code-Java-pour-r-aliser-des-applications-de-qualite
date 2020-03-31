154bae5d96fb3d13254fa7d69cd30c77184116b4
meta <gael.lehchibi@outlook.com>
2020-03-31 15:23:10 +0200

    implement cucumber

--- src/test/java/com/openclassrooms/testing/calcul/acceptance/CucumberAIT.java
+++ src/test/java/com/openclassrooms/testing/calcul/acceptance/CucumberAIT.java
@@ -1,11 +1,10 @@
 package com.openclassrooms.testing.calcul.acceptance;
 
-import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", plugin = { "pretty", "html:target/html-cucumber-report" })
+@CucumberOptions(features = "src/test/resources/features", plugin = {"pretty", "html:target/html-cucumber-report"})
 public class CucumberAIT {
 }
